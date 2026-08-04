63bc912a085785064b782f3d7cff8be244689d55
bran00 <hildebrando294@gmail.com>
2026-08-04 15:41:24 -0300

    feat: added callback logic

--- authentication-flow/implicit-flow/src/Callback.tsx
+++ authentication-flow/implicit-flow/src/Callback.tsx
@@ -1,4 +1,13 @@
+import { useLocation } from "react-router-dom"
+
 export function Callback() {
+ 
+  const { hash } = useLocation();
+
+  console.log(hash);
+
+
+
   return (
     <div>
       <h1>Callback</h1>
