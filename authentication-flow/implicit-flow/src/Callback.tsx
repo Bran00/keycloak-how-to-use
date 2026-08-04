b21c8e69bf4b75dc1daa2e3c94a12cc7a1522312
bran00 <hildebrando294@gmail.com>
2026-08-04 16:35:48 -0300

    feat: added callback authentication

--- authentication-flow/implicit-flow/src/Callback.tsx
+++ authentication-flow/implicit-flow/src/Callback.tsx
@@ -1,16 +1,25 @@
-import { useLocation } from "react-router-dom"
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom"
+import { login } from "./utils";
 
 export function Callback() {
- 
   const { hash } = useLocation();
+  const navigate = useNavigate();
 
-  console.log(hash);
+  useEffect(() => {
+    const searchParams = new URLSearchParams(hash.replace("#", ""));
+    const accessToken = searchParams.get("access_token") as string;
+    const idToken = searchParams.get("id_token") as string;
+    const state = searchParams.get("state") as string;
 
+    if(!accessToken || !idToken || !state) {
+      //navigate to login
+      navigate("/login");
+    }
 
+    login(accessToken, idToken, state);
 
-  return (
-    <div>
-      <h1>Callback</h1>
-    </div>
-  )
+  }, [hash, navigate]);
+
+  return <div>Loading...</div>
 }
--- authentication-flow/implicit-flow/src/utils.ts
+++ authentication-flow/implicit-flow/src/utils.ts
@@ -1,4 +1,5 @@
 import Cookies from "js-cookie";
+import { decodeJwt } from "jose";
 
 export function makeLoginUrl() {
   const nonce = Math.random().toString(36);
@@ -17,4 +18,33 @@ export function makeLoginUrl() {
   });
 
   return `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginUrlParams.toString()}`;
+}
+
+export function login(accessToken: string, idToken: string, state: string) {
+  const stateCookie = Cookies.get("state");
+  if (stateCookie !== state) {
+    throw new Error("Invalid state");
+  }
+
+  // eslint-disable-next-line
+  let decodedAccessToken = null;
+  // eslint-disable-next-line
+  let decodedIdToken = null;
+  try {
+    decodedAccessToken = decodeJwt(accessToken);
+    decodedIdToken = decodeJwt(idToken);
+    // eslint-disable-next-line
+  } catch (e) {
+    // eslint-disable-next-line
+    throw new Error("Invalid token");
+  }
+
+  if (decodedIdToken.nonce !== Cookies.get("nonce")) {
+    throw new Error("Invalid nonce");
+  }
+
+  Cookies.set("access_token", accessToken);
+  Cookies.set("id_token", idToken);
+
+  return decodedAccessToken;
 }
